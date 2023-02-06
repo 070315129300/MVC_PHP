68e0f9879d652e1f2dc7595b4f740832d5900024
k-leb <ikhidecaleb@gmail.com>
2023-02-06 09:13:21 +0100

    mvc

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+    // define db params
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'supermarket');
+    //app root
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+        //url root
+    define ('URLROOT', 'http://localhost/caymvc');
+
+    //site name
+    define('SITENAME', 'caymvc');
+
